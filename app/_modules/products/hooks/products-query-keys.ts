7dc23a1dca35a530ccac710ed0873f6960782a53
Mo'men <mmm.isam.99@gmail.com>
2026-09-23 16:01:59 +0300

    chore: fix lint issues and improve types

--- app/_modules/products/hooks/products-query-keys.ts
+++ app/_modules/products/hooks/products-query-keys.ts
@@ -1,6 +1,6 @@
 export const productsQueryKeys = {
   all: ["products"] as const,
-  filtered: (page?: number, limit?: number, catagoryid?: number) =>
-    [...productsQueryKeys.all, "filtered", page, limit, catagoryid] as const,
+  filtered: (page?: number, limit?: number, categoryId?: number) =>
+    [...productsQueryKeys.all, "filtered", page, limit, categoryId] as const,
   one: (id: number) => [...productsQueryKeys.all, "single", id] as const,
 };
